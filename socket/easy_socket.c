78ee43d78fee806b0766a291935d99a6d601d864
arkeopix <mendiej750@gmail.com>
2014-11-21 15:48:52 +0100

    linux ftw

--- socket/easy_socket.c
+++ socket/easy_socket.c
@@ -1,11 +1,24 @@
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
+#ifdef  _WIN32
+  #ifndef WIN32_LEAN_AND_MEAN
+  #define WIN32_LEAN_AND_MEAN
+  #endif
+  #include <windows.h>
+  #include <sys/socket.h>
+  #include <ws2tcpip.h>
+  #include <process.h>
+#elif __linux__
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netdb.h>
+  #include <sys/ioctl.h>
+  #include <netinet/in.h>
+  #include <netinet/ip.h>
+  #include <string.h>
+  #include <stdlib.h>
+  #include <errno.h>
 #endif
 #include <stdio.h>
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <process.h>
+
 #include "easy_socket.h"
 
 t_easy_socket *e_accept(t_easy_socket * this)
@@ -17,8 +30,8 @@ t_easy_socket *e_accept(t_easy_socket * this)
 	if (new->_socket =
 	    accept(this->_socket, (struct sockaddr *)&new->_client_addr,
 		   &size) == -1) {
-		fprintf(stderr, ERROR_ACCEPT, WSAGetLastError());
-		return NULL;
+	  fprintf(stderr, ERROR_ACCEPT), perror("accept");
+	  return NULL;
 	}
 	_init_func(new);
 	return new;
@@ -58,10 +71,17 @@ char *e_read(t_easy_socket * this)
 	unsigned long n = 0;
 	int total = 0, bytes;
 
+#ifdef _WIN32 
 	if (ioctlsocket(this->_socket, FIONREAD, &n) < 0) {
 		fprintf(stderr, ERROR_IOCTLSOCK, WSAGetLastError());
 		return NULL;
 	}
+#elif __linux__
+	if (ioctl(this->_socket, FIONREAD, &n) < 0) {
+		fprintf(stderr, ERROR_IOCTL), perror("ioctl");
+		return NULL;
+	}
+#endif
 	if ((ret = malloc(n * sizeof(*ret))) == NULL) {
 		fprintf(stderr, MALLOC_FAIL);
 		return NULL;
@@ -87,40 +107,47 @@ void _init_func(t_easy_socket * this)
 
 int socket_init(t_easy_socket * this, const char *port)
 {
+#ifdef _WIN32
 	WORD version = MAKEWORD(2, 2);
 	WSADATA ws_data;
+#endif
 	int status;
 	struct addrinfo hints;
 	struct addrinfo *serv_info;
 	struct addrinfo *tmp;
 
+#ifdef _WIN32
 	if ((WSAStartup(version, &ws_data)) != 0) {
 		fprintf(stderr, WINSOCK_INIT_FAIL, WSAGetLastError());
 		return -1;
 	}
-
+#endif
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	if ((status = getaddrinfo(NULL, port, &hints, &serv_info)) != 0) {
 		fprintf(stderr, ERROR_GETADDRINFO, gai_strerror(status));
+#ifdef _WIN32
 		WSACleanup();
+#endif
 		return -1;
 	}
 
 	for (tmp = serv_info; tmp != NULL; tmp = tmp->ai_next) {
 		if (this->_socket = socket(tmp->ai_family, tmp->ai_socktype,
-					   tmp->ai_protocol) ==
-		    INVALID_SOCKET) {
-			fprintf(stderr, ERROR_INIT_SOCKET, WSAGetLastError());
+					   tmp->ai_protocol) <
+		    0) {
+  fprintf(stderr, ERROR_INIT_SOCKET), perror("socket");
 		}
 	}
 
 	printf("socket = %d\n", this->_socket);
 	if (setsockopt(this->_socket, SOL_SOCKET, SO_REUSEADDR, (const char[]) {
 		       1}, sizeof(int)) != 0) {
-		fprintf(stderr, ERROR_SETSOCKOPT, WSAGetLastError());
+  fprintf(stderr, ERROR_SETSOCKOPT), perror("setsockopt");
+#ifdef _WIN32
 		WSACleanup();
+#endif
 		return -1;
 	}
 	printf("xouxou\n");
@@ -132,14 +159,18 @@ int socket_init(t_easy_socket * this, const char *port)
 
 	if (tmp == NULL) {
 		fprintf(stderr, BIND_FAIL, port);
+#ifdef _WIN32
 		WSACleanup();
+#endif
 		return -1;
 	}
 	freeaddrinfo(serv_info);
 
 	if (listen(this->_socket, BACKLOG) == -1) {
 		close(this->_socket);
+#ifdef _WIN32
 		WSACleanup();
+#endif
 		fprintf(stderr, ERROR_LISTEN);
 		return -1;
 	}
--- socket/easy_socket.h
+++ socket/easy_socket.h
@@ -1,17 +1,18 @@
-#ifndef         EASY_SOCKET_H_
+#ifndef        EASY_SOCKET_H_
 #define        EASY_SOCKET_H_
 
 #define        WINSOCK_INIT_FAIL "Winsock initialisation failed%d\n"
-#define        ERROR_INIT_SOCKET "Failed to create socket: %d\n"
-#define        ERROR_SETSOCKOPT  "Failed to set sockopt: %s\n"
+#define        ERROR_INIT_SOCKET "Failed to create socket\n"
+#define        ERROR_SETSOCKOPT  "Failed to set sockopt\n"
 #define        ERROR_LISTEN      "Failed to listen\n"
 #define        BIND_FAIL         "Socket failed to bind socket to port %d\n"
 #define        ERROR_GETADDRINFO "GetAddrInfo failed: %s\n"
-#define        ERROR_ACCEPT      "Accept failed: %d\n"
+#define        ERROR_ACCEPT      "Accept failed\n"
 #define        ERROR_STRLEN      "You have to provide a string\n"
 #define        ERROR_SEND        "Send failed\n"
 #define        ERROR_RECV        "Recv failed\n"
 #define        ERROR_IOCTLSOCK   "Ioctlsocket failed to assert socket data: %d\n"
+#define        ERROR_IOCTL	 "Ioctl failed to assert socket data\n"
 #define        MALLOC_FAIL       "Could not alocate memory for buffer\n"
 #define        BACKLOG           10
 #define        BUFF_SIZE         124
@@ -24,11 +25,11 @@ typedef struct s_easy_socket {
 	int (*e_write) (t_easy_socket *, const char *);
 	char *(*e_read) (t_easy_socket *);
 
-	SOCKET _socket;
+	int    _socket;
 	struct sockaddr_in _client_addr;
 } t_easy_socket;
 
 int socket_init(t_easy_socket *, const char *);
 void _init_func(t_easy_socket *);
 
-#endif				/* !EASY_SOCKET_H_ */
+#endif				/* !EASY_SOCKETH_ */
