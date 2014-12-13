5edd5b1d554cbf1add74b4395c13e9f9ad39f27a
Julien ganichot <julien.ganichot@gmail.com>
2014-12-13 23:16:12 +0100

    Working well

--- list/list.c
+++ list/list.c
@@ -2,52 +2,86 @@
 #include <stdio.h>
 #include <string.h>
 
-int		easy_list_size(t_list **self)
-{
-  int		i;
-  t_list	*tmp;
+#include "list.h"
 
+t_list* easy_list_remove_first(t_list** self)
+{
+  t_list*  tmp;
+
+  if (!(*self))
+    return (NULL);
   tmp = (*self);
-  i = 0;
-  while (tmp)
-    {
-      ++i;
-      tmp = tmp->next;
-    }
-  return (i);
+  (*self) = (*self)->next;
+  if (tmp->data)
+    free(tmp->data);
+  free(tmp);
+  tmp = NULL;
+  if (*self)
+    (*self)->size--;
+  return (*self);
 }
 
-t_list*		easy_list_remove_at(t_list **self, unsigned int n)
+t_list* easy_list_free_node(t_list* node)
+{
+  node->next = NULL;
+  if (node->data)
+  {
+    free(node->data);
+    node->data = NULL;
+  }
+  free(node);
+  node = NULL;
+  return (node);
+}
+
+t_list* easy_list_remove_last(t_list** self)
+{
+  t_list*  tmp;
+  t_list*  to_rm;
+
+  if (!(*self))
+    return (NULL);
+  tmp = (*self);
+  if ((*self)->next == NULL)
+    return (easy_list_free_node(*self));
+  while (tmp && tmp->next && tmp->next->next)
+    tmp = tmp->next;
+  to_rm = tmp->next;
+  easy_list_free_node(to_rm);
+  tmp->next = NULL;
+  (*self)->size--;
+  return (*self);
+}
+
+t_list*   easy_list_remove_at(t_list** self, unsigned int n)
 {
   t_list	*tmp;
   t_list	*to_rm;
-  unsigned int	i;
 
-  i = 0;
   tmp = (*self);
-  to_rm = NULL;
-  while (tmp && i < n)
-    {
-      tmp = tmp->next;
-      ++i;
-    }
-  if (tmp)
-    {
-      to_rm = tmp;
-      to_rm->next = NULL;
-      tmp = (tmp->next) ? tmp->next : NULL;
-      free(to_rm);
-      to_rm = NULL;
-      return (*self);
-    }
-  return (NULL);
+
+  if (!(*self))
+    return (NULL);
+  else if (n == 0)
+    return (easy_list_remove_first(self));
+  else if (n == (*self)->size - 1)
+    return (easy_list_remove_last(self));
+  else if (n > (*self)->size - 1)
+    return (NULL);
+  for (unsigned int i = 0 ; i != n - 1 ; ++i)
+    tmp = tmp->next;
+  to_rm = tmp->next;
+  tmp->next = tmp->next->next;
+  easy_list_free_node(to_rm);
+  (*self)->size--;
+  return (*self);
 }
 
-t_list*		easy_list_remove(t_list **self, void *data)
+t_list*   easy_list_remove(t_list** self, void* data)
 {
-  t_list	*tmp;
-  t_list	*to_rm;
-  
+  t_list*  tmp;
+  t_list*  to_rm;
+
   tmp = (*self);
   to_rm = NULL;
   if (tmp && tmp->data == data)
@@ -59,50 +93,59 @@ t_list*		easy_list_remove(t_list **self, void *data)
   else
     {
       while (tmp && tmp->next && tmp->next != data)
-	tmp = tmp->next;
+        tmp = tmp->next;
       if (tmp && tmp->next)
-	{
-	  to_rm = tmp;
-	  tmp->next = tmp->next->next;
-	  free(to_rm);
-	  to_rm = NULL;
-	}
+        {
+          to_rm = tmp;
+          tmp->next = tmp->next->next;
+          free(to_rm);
+          to_rm = NULL;
+        }
     }
   return (*self);
 }
 
-bool		easy_list_is_empty(t_list **self)
+bool    easy_list_is_empty(t_list** self)
 {
-  if ((*((*self)->size))(self) > 0)
+  if ((*self)->size > 0)
     return (false);
   return (true);
 }
 
-void*		easy_list_add(t_list **self, void* data)
+void*   easy_list_add(t_list** self, void* data)
 {
-  t_list	*elem = NULL;
+  t_list*  elem = NULL;
 
-  if ((*self) && ((*((*self)->size))(self) - 1) == 0)
-    elem = (*self);
-  else if ((elem = easy_list_construct(&elem)) == NULL)
+  if (!(*self))
     return (NULL);
-  elem->data = data;  
-  elem->next = ((*((*self)->size))(self) > 0) ? (*self) : NULL;
-  elem->empty_node = (data == NULL) ? true : false;
+  else
+    if ((*self)->empty_node == true)
+      {
+        (*self)->data = data;
+        (*self)->size++;
+        (*self)->empty_node = false;
+        return (*self);
+      }
+  if ((elem = easy_list_constructor(&elem)) == NULL)
+    return (NULL);
+  elem->size = (*self)->size + 1;
+  elem->data = data;
+  elem->next = (*self);
+  elem->empty_node = false;
   (*self) = elem;
   return (*self);
 }
 
-void		easy_list_clear(t_list **self)
+void    easy_list_clear(t_list** self)
 {
-  t_list	*tmp = NULL;
+  t_list*  tmp = NULL;
 
   while (*self)
     {
       tmp = (*self);
       (*self) = (*self)->next;
       if (tmp->data)
-	free(tmp->data);
+        free(tmp->data);
       tmp->add = NULL;
       tmp->clear = NULL;
       tmp->data = NULL;
@@ -110,15 +153,15 @@ void		easy_list_clear(t_list **self)
       free(tmp);
       tmp = NULL;
     }
-  (*self) = easy_list_construct(self);
+  (*self) = easy_list_constructor(self);
 }
 
-t_list*		easy_list_construct(t_list **list)
+t_list*   easy_list_constructor(t_list** list)
 {
   if (!(*list))
     {
       if (((*list) = malloc(sizeof(t_list))) == NULL)
-	return (NULL);
+        return (NULL);
     }
   (*list)->empty_node = true;
   (*list)->remove = &easy_list_remove;
@@ -126,9 +169,11 @@ t_list*		easy_list_construct(t_list **list)
   (*list)->clear = &easy_list_clear;
   (*list)->add = &easy_list_add;
   (*list)->is_empty = &easy_list_is_empty;
-  (*list)->size = &easy_list_size;
+  (*list)->remove_first = &easy_list_remove_first;
+  (*list)->remove_last = &easy_list_remove_last;
   (*list)->data = NULL;
   (*list)->next = NULL;
+  (*list)->size = 0;
   return (*list);
 }
 
--- list/list.h
+++ list/list.h
@@ -1,4 +1,4 @@
-#ifndef
+#ifndef LIST_H_
 # define LIST_H_
 
 # define bool	unsigned char
@@ -7,23 +7,26 @@
 
 typedef struct		s_list
 {
+  unsigned int		size;
   bool			empty_node;
-  int			(*size)(struct s_list**);
   void*			(*add)(struct s_list**, void*);
   void			(*clear)(struct s_list**);
   bool			(*is_empty)(struct s_list**);
   struct s_list*	(*remove)(struct s_list**, void*);
   struct s_list*	(*remove_at)(struct s_list**, unsigned int);
+  struct s_list*	(*remove_last)(struct s_list**);
+  struct s_list*	(*remove_first)(struct s_list**);
   void			*data;
   struct s_list		*next;
 }			t_list;
 
-int		easy_list_size(t_list **self);
-void*		easy_list_add(t_list **self, void *data);
-void		easy_list_clear(t_list **self);
-bool		easy_list_is_empty(t_list **self);
-t_list*		easy_list_remove(t_list **self, void *data);
-t_list*		easy_list_remove_at(t_list **self, unsigned int n);
-t_list*		easy_list_construct(t_list **list);
+void*			easy_list_add(t_list **self, void *data);
+void			easy_list_clear(t_list **self);
+bool			easy_list_is_empty(t_list **self);
+t_list*			easy_list_remove(t_list **self, void *data);
+t_list*			easy_list_remove_last(t_list **self);
+t_list*			easy_list_remove_first(t_list **self);
+t_list*			easy_list_remove_at(t_list **self, unsigned int n);
+t_list*			easy_list_constructor(t_list **list);
 
 #endif /* !LIST_H_ */
