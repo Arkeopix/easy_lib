927d8ed77d2af7535b7311717991a463ef5b18fb
arkeopix <mendiej750@gmail.com>
2015-01-14 13:16:49 +0100

    back to work

--- sdl/easy_sdl.c
+++ sdl/easy_sdl.c
@@ -18,17 +18,41 @@ int		t_sdl_init(t_sdl *this, const int init_mod, const char *name,
       this->_surface = SDL_GetWindowSurface(this->_window);
     }
   }
+  return 0;
 }
 
 int		t_sdl_destroy(t_sdl *this) {
   SDL_DestroyWindow(this->_window);
   SDL_Quit();
+  return 0;
+}
+
+int		t_sdl_load_media(SDL_Surface *surface, const char *path) {
+  
+
+  if ((surface = SDL_LoadBMP(path)) == NULL) {
+    fprintf(stderr, ERROR_LOADMEDIA, SDL_GetError());
+    return -1;
+  }
+  return 0;
+}
+
+int		blit_surface(t_sdl *this, SDL_Surface *surface) {
+  if ((SDL_BlitSurface(surface, NULL, this->_surface, NULL)) < 0) {
+    fprintf(stderr, ERROR_BLITSURFACE, SDL_GetError());
+    return -1;
+  }
+  SDL_UpdateWindowSurface(this->_window);
+  return 0;
 }
 
 int main() {
   t_sdl test;
 
   t_sdl_init(&test, VIDEO, "test", 500, 500);
+  SDL_Surface image_test;
+  t_sdl_load_BMP(&image_test, "index.jpeg");
+  test.blit_surface(&test, &image_test);
   t_sdl_destroy(&test);
   return 0;
 }
--- sdl/easy_sdl.h
+++ sdl/easy_sdl.h
@@ -7,7 +7,9 @@
 # define	SOUND	2
 
 # define	ERROR_SDL_INIT		"SDL could not initialize: error %s\n"
-# define	ERROR_WINDOW_CREATE	"SDL could not create window: erroe %s\n"
+# define	ERROR_WINDOW_CREATE	"SDL could not create window: error %s\n"
+# define	ERROR_LOADMEDIA		"SDL could not load media: error %s\n"
+# define	ERROR_BLITSURFACE	"SDL could not blit surface: error %s\n"
 
 typedef struct	s_sdl t_sdl;
 
@@ -15,9 +17,11 @@ typedef struct	s_sdl {
   SDL_Window	*_window;
   SDL_Surface	*_surface;
   
+  int		(*blit_surface)(t_sdl *, SDL_Surface *);
 }		t_sdl;	
 
 int		t_sdl_init(t_sdl *, const int, const char*, const int, const int);
 int		t_sdl_destroy(t_sdl *);
+int		t_sdl_load_media(SDL_Surface *, const char *);
 
 #endif		/* !EASY_SDL */
